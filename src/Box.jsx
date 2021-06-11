9cd0a94f8955bfbf6bd00de0eb21bacb5c88055e
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2021-06-12 00:43:48 +0900

    React DnD: 単純なドラッグ&ドロップ 04

--- src/Box.jsx
+++ src/Box.jsx
@@ -10,11 +10,17 @@ const style = {
 	width: 'max-content',
 };
 const role = 'Box';
-export const Box = ({ id, left, top, children }) => {
-	const [, drag] = useDrag(() => ({
+export const Box = ({ id, left, top, hideSourceOnDrag, children, }) => {
+	const [{ isDragging }, drag] = useDrag(() => ({
 		type: ItemTypes.BOX,
 		item: { id, left, top },
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging(),
+		}),
 	}), [id, left, top]);
+	if (isDragging && hideSourceOnDrag) {
+		return <div ref={drag}/>;
+	}
 	return (
 		<div ref={drag} style={{ ...style, left, top }} role={role}>
 			{children}
--- src/Example.jsx
+++ src/Example.jsx
@@ -1,9 +1,20 @@
+import { useState, useCallback } from 'react';
 import { Container } from './Container';
 
 export const Example = () => {
+	const [hideSourceOnDrag, setHideSourceOnDrag] = useState(true);
+	const toggle = useCallback(() => setHideSourceOnDrag(!hideSourceOnDrag), [
+		hideSourceOnDrag,
+	]);
 	return (
 		<div>
-			<Container />
+			<Container hideSourceOnDrag={hideSourceOnDrag} />
+			<p>
+				<label htmlFor="hideSourceOnDrag">
+					<input id="hideSourceOnDrag" type="checkbox" checked={hideSourceOnDrag} onChange={toggle}/>
+					<small>Hide the source item while dragging</small>
+				</label>
+			</p>
 		</div>
 	);
 };
