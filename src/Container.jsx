d4d2dc9d0f702585559d0137bc2c55a8e7977dd1
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2021-06-11 22:37:45 +0900

    Container.jsxの微修正

--- src/Container.jsx
+++ src/Container.jsx
@@ -9,7 +9,6 @@ const styles = {
 	border: '1px solid black',
 	position: 'relative',
 };
-// export const Container = () => {
 export const Container = ({ hideSourceOnDrag }) => {
 	const [boxes, setBoxes] = useState({
 		a: { top: 20, left: 80, title: 'Drag me around' },
@@ -25,9 +24,9 @@ export const Container = ({ hideSourceOnDrag }) => {
 	const [, drop] = useDrop(() => ({
 		accept: ItemTypes.BOX,
 		drop(item, monitor) {
-			const delta = monitor.getDifferenceFromInitialOffset();
-			const left = Math.round(item.left + delta.x);
-			const top = Math.round(item.top + delta.y);
+			const { x, y } = monitor.getDifferenceFromInitialOffset();
+			const left = Math.round(item.left + x);
+			const top = Math.round(item.top + y);
 			moveBox(item.id, left, top);
 			return undefined;
 		},
@@ -37,7 +36,6 @@ export const Container = ({ hideSourceOnDrag }) => {
 			{Object.keys(boxes).map((key) => {
 				const { left, top, title } = boxes[key];
 				return (
-					// <Box key={key} id={key} left={left} top={top}>
 					<Box key={key} id={key} left={left} top={top} hideSourceOnDrag={hideSourceOnDrag}>
 						{title}
 					</Box>
